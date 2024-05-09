3c1c2aebf13b29b0b098b3ae7cf0416a0fbb097c
Red Butay <redickbutay02@gmail.com>
2024-05-10 00:02:18 +0800

    added base_person module

---
+++ base_person/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Base Person module",
+    "author": "reichie020212",
+    "website": "https://github.com/reichie020212/red-modules",
+    "category": "Self-Developed Modules",
+    "version": "17.0.1.0.0",
+    "depends": ["base"],
+    "data": [
+        # "security/ir.model.access.csv",
+    ],
+    "application": False,
+    "installable": True,
+    "auto_install": False,
+    "maintainer": "reichie020212",
+    "license": "LGPL-3",
+    "assets": {},
+}
